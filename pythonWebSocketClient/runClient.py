e42cba34e5717662598020a316b43a5cbb04e51c
Janeusz2000 <Mateusz.krzysztof.janik@gmail.com>
2024-12-28 00:18:50 +0100

    initial commit - we are going with rust baby

---
+++ pythonWebSocketClient/runClient.py
@@ -0,0 +1,83 @@
+import argparse
+import asyncio
+import websockets
+from typing import Final
+
+URI: Final = "ws://127.0.0.1:8080/ws"  # WebSocket server URL
+
+async def websocket_send(message: str, close_event):
+    try:
+        async with websockets.connect(URI, ping_interval=20, ping_timeout=20) as websocket:
+            print(f"WebSocket server=\"{URI}\" CONNECTED")
+            print(f"Message=\"{message}\" sent!")
+
+            while not close_event.is_set():
+                try:
+                    await asyncio.wait_for(websocket.send(message), timeout=1.0)
+                    break
+
+                except asyncio.TimeoutError:
+                    print("did not succeded with sending the message, trying again in 1s")
+                    await asyncio.sleep(1)
+
+
+    except websockets.ConnectionClosed as e:
+        print(f"Connection closed: {e}")
+    finally:
+        if not close_event.is_set():
+            close_event.set()
+        print(f"WebSocket server=\"{URI}\" DISCONNECTED")
+
+async def websocket_receive(close_event):
+    try:
+        async with websockets.connect(URI, ping_interval=10, ping_timeout=20) as websocket:
+            print(f"WebSocket server=\"{URI}\" CONNECTED")
+            while not close_event.is_set():
+                try:
+                    response = await asyncio.wait_for(websocket.recv(), timeout=2.0)
+                    print(response)
+
+                except asyncio.TimeoutError:
+                    # Continue checking if we need to close the connection
+                    pass
+    except websockets.ConnectionClosed as e:
+        print(f"Connection closed: {e}")
+    finally:
+        print(f"WebSocket server=\"{URI}\" DISCONNECTED")
+
+async def stop_websocket(close_event):
+    try:
+        while not close_event.is_set():
+            await asyncio.sleep(1)  # Keep the loop running
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt detected, stopping...")
+        close_event.set()
+
+async def main():
+    description = """
+    Script that sends or receives websocket messages for testing purposes.
+    """
+    parser = argparse.ArgumentParser(description)
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    group.add_argument("-s", "--send", type=str, help="Send message to the websocket", required=False)
+    group.add_argument("-l", "--listen", action="store_true", help="Listen to the websocket events", required=False)
+
+    args = parser.parse_args()
+
+    if args.send:
+        close_event = asyncio.Event()
+        sending_tasks = asyncio.create_task(websocket_send(args.send, close_event))
+        stop_task = asyncio.create_task(stop_websocket(close_event))
+        await asyncio.gather(sending_tasks, stop_task)
+
+    if args.listen:
+        close_event = asyncio.Event()
+        listen_task = asyncio.create_task(websocket_receive(close_event))
+        stop_task = asyncio.create_task(stop_websocket(close_event))
+
+        # Wait for both tasks to complete
+        await asyncio.gather(listen_task, stop_task)
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ twitch_chess/src/main.rs
@@ -0,0 +1,138 @@
+use actix_web::middleware::Logger;
+use actix_web::{rt, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_ws::Message;
+use colored::*;
+use std::io::Write;
+use tokio::sync::broadcast;
+use tokio::time::{self, Duration};
+
+use futures_util::StreamExt;
+
+struct AppState {
+    sender: broadcast::Sender<String>,
+}
+
+fn initialize_logger() {
+    env_logger::Builder::new()
+        .format(|buf, record| {
+            let level_color = match record.level() {
+                log::Level::Error => Color::Red,
+                log::Level::Warn => Color::Yellow,
+                log::Level::Info => Color::Green,
+                log::Level::Debug => Color::Cyan,
+                log::Level::Trace => Color::Magenta,
+            };
+
+            std::writeln!(
+                buf,
+                "{} [{}] {}",
+                chrono::Local::now()
+                    .format("%Y-%m-%d %H:%M:%S")
+                    .to_string()
+                    .color(Color::Cyan),
+                record.level().to_string().color(level_color),
+                record.args()
+            )
+        })
+        .filter(None, log::LevelFilter::Info)
+        .init();
+}
+
+async fn ws(
+    req: HttpRequest,
+    stream: web::Payload,
+    state: web::Data<AppState>,
+) -> Result<HttpResponse, Error> {
+    let (res, mut session, mut ws_stream) = actix_ws::handle(&req, stream)?;
+    let mut receiver = state.sender.subscribe();
+
+    log::info!(
+        "WebSocket connected client={:?}, req={:?} res={:?}",
+        receiver,
+        req,
+        res
+    );
+
+    // Spawn a task to listen for broadcast messages and send them to the client
+    rt::spawn(async move {
+        let mut ping_interval = time::interval(Duration::from_secs(10));
+        loop {
+            tokio::select! {
+                // Listen for broadcast messages
+                _ = ping_interval.tick() => {
+                                    if session.ping(b"ping").await.is_err() {
+                                        log::warn!("Failed to send ping, closing connection");
+                                        break;
+                                    }
+                                },
+                Ok(msg) = receiver.recv() => {
+                    if session.text(msg).await.is_err() {
+                        log::warn!("Failed to send message");
+                        break;
+                    }
+                }
+                // Monitor for WebSocket stream termination
+                Some(Ok(msg)) = ws_stream.next() => {
+                    match msg {
+                        Message::Close(msg) => {
+                            log::info!("WebSocket session closed msg={:?}", msg);
+                            break;
+                        }
+                        Message::Text(text) => {
+                            let _ = state.sender.send(text.to_string());
+                            println!("{}", text);
+                        }
+                        Message::Ping(_) => {
+                            println!("ping");
+                        }
+                        Message::Pong(pong_data) => {
+                            println!("{:?}", pong_data);
+
+                        }
+                        _ => {
+                            // Handle other types of messages if needed
+                        }
+                    }
+                }
+                else => {
+                    log::info!("WebSocket stream ended");
+                    break;
+                }
+            }
+        }
+    });
+
+    Ok(res)
+}
+
+async fn broadcast_timestamps(sender: broadcast::Sender<String>) {
+    let mut interval = time::interval(Duration::from_secs(1));
+    loop {
+        interval.tick().await;
+        let timestamp = chrono::Local::now().to_rfc3339();
+        let _ = sender.send(timestamp);
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    initialize_logger();
+
+    let (sender, _receiver) = broadcast::channel(100);
+    let state = web::Data::new(AppState { sender });
+    let sender_clone = state.sender.clone();
+
+    rt::spawn(async move {
+        broadcast_timestamps(sender_clone).await;
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(state.clone())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .route("/ws", web::get().to(ws))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
